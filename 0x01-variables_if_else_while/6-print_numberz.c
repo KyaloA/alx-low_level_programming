816fcc24390b4984626713124a4cad8f8d045800
KyaloA <allankyalo04@gmail.com>
2022-09-12 16:34:22 -0700

    commit 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int c =0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
