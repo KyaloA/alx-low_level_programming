ca8abc05b39dd154c98ba3fccee5c23e72fdfa42
KyaloA <allankyalo04@gmail.com>
2022-09-12 16:54:19 -0700

    commit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int l, m;
+	for (l = 0; l < 10; l++0)
+	{
+		putchar (l + '0');
+	}
+	for (m = 'a'; m <= 'f'; m++)
+	{
+		putchar (m);
+	}
+	putchar ('\n');
+	return (0);
+}
