7d0bc33249a8883a6b23879b9c5ff7766f9521bf
KyaloA <allankyalo04@gmail.com>
2022-09-16 09:26:09 -0700

    corrections adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_number - print an int numbers
- * @n: number tested
- * Return: Always 0
+ * print_number - print an int numbers.
+ * @n: number tested.
+ * Return: Always 0.
  */
 void print_number(int n)
 {
