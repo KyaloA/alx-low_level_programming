f34a0f8432719ec8204bdda583800b41eb5f5550
KyaloA <allankyalo04@gmail.com>
2022-09-15 20:12:10 -0700

    correction 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: a character to be checked on
  * Return: returns 1 or 0 depending on condition
  */
-int -isalpha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'));
 }
