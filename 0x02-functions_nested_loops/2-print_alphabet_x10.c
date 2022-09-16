7e3a6b446292a10db374cd840115f666a5911584
KyaloA <allankyalo04@gmail.com>
2022-09-15 18:20:21 -0700

    third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lower case alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
