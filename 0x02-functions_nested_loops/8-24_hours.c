0f8ee415e4d75b8f9bbba174a40388b0c605ec44
KyaloA <allankyalo04@gmail.com>
2022-09-15 20:40:42 -0700

    correction 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@ void jack_bauer(void)
 {
 	int x, y;
 
-	for (x = 0; x < 21; x++)
+	for (x = 0; x < 24; x++)
 	{
 		for (y = 0; y < 60; y++)
 		{
@@ -36,4 +36,3 @@ void jack_bauer(void)
 		}
 	}
 }
-				
