cf93dadc60db6af7f9e3b57f1b219e35c8d1feba
KyaloA <allankyalo04@gmail.com>
2022-09-22 11:34:36 -0700

    correction 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	char *temp = dest;
 
-	while (dest[len])
-			len++;
+	while (*dest)
+		dest++;
 
-			for (i = 0; src[i] != '\0'; i++)
-			{
-			dest[len] = src[i];
-			len += 1;
-			}
-			dest[len] = '\0';
-			return (dest);
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+			return (temp);
 }
